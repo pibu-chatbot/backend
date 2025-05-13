7626466fbbea22fa13b23ea175ac00299109287e
yugyeongh <uugyeong27@gmail.com>
2025-05-13 11:47:09 +0900

    [hotfix] draft main.py

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
-from schemas.request_response import QueryRequest, QueryResponse
-from services.answer_service import get_final_answer
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+from models.embedder import embed_text
 
 app = FastAPI()
 
-@app.post("/ask", response_model=QueryResponse)
-async def ask_query(req: QueryRequest):
-    return get_final_answer(req.query)
+# CORS 설정 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/ask")
+def ask_question(request: QueryRequest):
+    # answer = embed_text(request.query)
+    return {"answer": request.query}
