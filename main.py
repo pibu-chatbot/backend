6ab8b298b222847d96fb74977b6bc6e4035f618a
yugyeongh <uugyeong27@gmail.com>
2025-05-16 17:18:12 +0900

    [refactor] main.py to clean

--- main.py
+++ main.py
@@ -1,61 +1,59 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from transformers import AutoTokenizer, AutoModelForCausalLM
-from peft import PeftModel
-from langchain_core.messages import HumanMessage
-from request_response import get_chain_with_model
-import torch
-import os
-from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
 
-load_dotenv()
+from models.embedder import embed_text
+from schemas.request_response import get_chain
 
-hf_token = os.getenv("HF_TOKEN")
-openai_token = os.getenv("OPENAI_API_KEY")
 
-app = FastAPI()
-
-# CORS 설정
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# 모델 및 토크나이저 로드
-base_model_path = "mistralai/Mistral-7B-Instruct-v0.1"
-adapter_path = "./qlora_model_mistral"
-
-base_model = AutoModelForCausalLM.from_pretrained(
-    base_model_path,
-    device_map="auto",
-    torch_dtype=torch.float16,
-    use_auth_token=hf_token
-)
-
-# 2. adapter 병합
-model = PeftModel.from_pretrained(base_model, adapter_path)
-model = model.merge_and_unload() 
-
-# 3. tokenizer도 base 모델 기준
-tokenizer = AutoTokenizer.from_pretrained(base_model_path)
-
-# LangChain용 chain 생성
-chain_with_history = get_chain_with_model(model)
-
-# 입력 스키마 정의
 class QueryRequest(BaseModel):
     query: str
-    session_id: str = "default"
 
-# API 엔드포인트
+def load_openai_api_key():
+    load_dotenv()
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+    return openai_api_key
+
+def set_cors(app):
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+def build_model():
+    model = ChatOpenAI(
+        model_name='gpt-4o-mini', 
+        temperature=0.4, 
+        token=load_openai_api_key()
+    )
+    return model
+
+def create_app():
+    app = FastAPI() 
+    load_openai_api_key()
+    set_cors(app)
+    return app
+
+app = create_app()
+
+if __name__=='__main__':
+    
+    model = build_model()
+    chat_memory = get_chain(model)
+
+
+
 @app.post("/ask")
 def ask_question(request: QueryRequest):
-    result = chain_with_history.invoke(
-        {"query": request.query},
-        config={"configurable": {"session_id": request.session_id}}
-    )
-    return {"answer": result.content}
+    # answer = embed_text(request.query)
+    response = chain_with_history.invoke({"query": request.query}, config={"configurable": {"session_id": "test"}})
+    print(response)
+    return {"answer": response.content}
+
+# uvicorn main:app --reload
